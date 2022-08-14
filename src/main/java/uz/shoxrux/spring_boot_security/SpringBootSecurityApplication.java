30b0bd352c606fae3fa066c9426a84875a28e8de
ultron005 <begratefuller005@gmail.com>
2022-08-14 16:01:46 +0500

    go go

--- src/main/java/uz/shoxrux/spring_boot_security/SpringBootSecurityApplication.java
+++ src/main/java/uz/shoxrux/spring_boot_security/SpringBootSecurityApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityApplication.class, args);
+    }
 
 }
